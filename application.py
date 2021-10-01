c152fc0c8f34ecc15b5afeb79285e0f521124bf4
kamranfotovat <87545020+kamranfotovat@users.noreply.github.com>
2021-10-01 14:21:28 +0330

    delete application.py

--- application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request,render_template,redirect
-
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "tiams PhoneNumber: haooou AND kamys PhoneNumber is : 09175118377 AND rojas PhoneNumber is: 652"
