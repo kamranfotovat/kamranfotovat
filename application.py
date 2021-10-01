97896fad346ce90ac081a7ec809d7c0826fe371e
kian <kianof2018@gmail.com>
2021-10-01 18:56:17 +0000

    fixing bugs and adding apology , unknown. upd application.py

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, flash, redirect, render_template, request, session
+from cs50 import SQL
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from unknown import apology, login_required
 
 app = Flask(__name__)
 
