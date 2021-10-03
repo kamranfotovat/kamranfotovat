32e3c38b28850cdb6ed179aafd1d06c0409e9b59
kamranfotovat <kamran.fotovvat2018@gmail.com>
2021-10-03 21:37:20 +0330

    login application half

--- application.py
+++ application.py
@@ -8,3 +8,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "post":
+        pass
+
+    if request.method == "get":
+        return render_template("login.html")
