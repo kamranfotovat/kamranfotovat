2652ef1916fae92d830d392f535d8ac5644f4b5e
kian <kianof2018@gmail.com>
2021-10-03 19:35:27 +0000

    filling application + adding barca.db

--- application.py
+++ application.py
@@ -12,8 +12,46 @@ def index():
 
 @app.route("/login", methods=["POST","GET"])
 def login():
+
     if request.method == "post":
-        pass
+
+        name = request.form.get("username")
+        password = request.g=form.get("password")
+
+
+        if not name:
+            pass
+
+        if not password:
+            pass
+
 
     if request.method == "get":
         return render_template("login.html")
+
+@app.route("/register", methods=["POST","GET"])
+def register():
+    if request.method == "post":
+
+        name = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not name:
+            pass
+
+        if not password:
+            pass
+
+        if not confirmation:
+            pass
+
+        if password != confirmation:
+            pass
+
+
+
+
+
+    if request.method == "get":
+        return render_template("register.html")
