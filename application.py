09dfab90069281c4a150e5aad2277ecd9a9ab44a
kian <kianof2018@gmail.com>
2021-10-03 21:20:15 +0330

    a small change

--- application.py
+++ application.py
@@ -1,10 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, session
-from cs50 import SQL
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from unknown import apology, login_required
+
+
 
 app = Flask(__name__)
 
